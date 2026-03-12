3106041808f087ae848b5acf1ce7f06e744044f1
gvivek125 <gvivek1225@gmail.com>
2026-03-13 01:51:46 +0530

    login api added

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,24 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
+// server check route
 app.get("/", (req,res)=>{
   res.send("Backend working successfully 🚀")
+})
+
+// login route
+app.post("/login",(req,res)=>{
+
+  const {username,password} = req.body
+
+  if(username === "admin" && password === "1234"){
+    res.send("success")
+  }else{
+    res.send("fail")
+  }
+
 })
 
 const PORT = process.env.PORT || 5000
