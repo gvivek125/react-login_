f641017f49b070f533890e5954987fe606105b0c
gvivek125 <gvivek1225@gmail.com>
2026-03-13 01:27:22 +0530

    added test route

--- back-end/index.js
+++ back-end/index.js
@@ -5,3 +5,17 @@ const app=express()
 app.listen(5000,()=>{
     console.log("server is get ready ....")
 })
+
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req,res)=>{
+  res.send("Backend working successfully 🚀")
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("server running")
+})
